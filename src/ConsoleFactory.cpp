af8df5fdce8a58348da561d3bb887ef0d5a445ac
Paweł Rejkowicz <motorek1991@gmail.com>
2017-12-21 14:08:43 +0100

    adding support for handlers

--- src/ConsoleFactory.cpp
+++ src/ConsoleFactory.cpp
@@ -1,35 +1,96 @@
+#include <functional>
 #include <iostream>
+#include <optional>
 #include <reyco/ConsoleFactory.h>
+#include <unordered_map>
 
 namespace reyco {
 
 class Console {
 public:
+  class CmdHandler;
+
   Console(std::istream &in, std::ostream &out) : in(in), out(out) {}
-  bool start() {
-    std::string cmd = "restart";
-    while (true) {
-      out << '\n' << cmd << ">> ";
-      if (!(in >> cmd))
-        return false;
-      if (!cmd.compare("exit"))
-        return false;
-      if (!cmd.compare("restart"))
-        return true;
+  bool start();
 
-      if (!cmd.compare("nop"))
+  auto addCommand(const std::string_view &cmd, auto handler);
+
+private:
+  Console &registerCmd(const std::string_view &cmd, CmdHandler &handler) {
+    cmdHandlers.emplace(cmd, std::ref(handler));
+    return *this;
+  }
+  void unregisterCmd(const std::string_view &cmd) { cmdHandlers.erase(cmd); }
+
+  std::istream &in;
+  std::ostream &out;
+  using CmdHandlerRef = std::reference_wrapper<CmdHandler>;
+  std::unordered_map<std::string_view, CmdHandlerRef> cmdHandlers;
+};
+
+class Console::CmdHandler {
+public:
+  CmdHandler(const std::string_view &cmd, Console &console)
+      : cmd(cmd), console(console.registerCmd(cmd, *this)) {}
+  virtual ~CmdHandler() { console.unregisterCmd(cmd); }
+
+  virtual std::optional<bool> handle() = 0;
+
+protected:
+  std::string_view cmd;
+  Console &console;
+};
+
+bool Console::start() {
+  std::string cmd = "(started)";
+  while (true) {
+    out << '\n' << cmd << ">> ";
+    if (!(in >> cmd))
+      return false;
+
+    try {
+      CmdHandler &handler = cmdHandlers.at(cmd).get();
+      std::optional<bool> result = handler.handle();
+
+      if (!result.has_value())
         continue;
-
+      return *result;
+    } catch (std::out_of_range) {
       using namespace std::string_literals;
       throw std::runtime_error("no such command: '"s + cmd + "'"s);
     }
-    return false;
   }
+  return false;
+}
 
-private:
-  std::istream &in;
-  std::ostream &out;
-};
+auto Console::addCommand(const std::string_view &cmd, auto handler) {
+
+  constexpr bool isVoid = std::is_void_v<decltype(handler(in, out))>;
+  constexpr auto innerHandler = handler;
+  if
+    constexpr(isVoid) {
+      class InlineCmdHandler : public Console::CmdHandler {
+      public:
+        using CmdHandler::CmdHandler;
+        std::optional<bool> handle() override {
+          innerHandler(console.in, console.out);
+          return {};
+        }
+      };
+      return std::make_unique<InlineCmdHandler>(cmd, *this);
+    }
+  else {
+    constexpr auto recipe = handler;
+    class InlineCmdHandler : public Console::CmdHandler {
+    public:
+      using CmdHandler::CmdHandler;
+      std::optional<bool> handle() override {
+        return innerHandler(console.in, console.out);
+      }
+    };
+    return std::make_unique<InlineCmdHandler>(cmd, *this);
+  }
+}
 
 ConsoleFactory::~ConsoleFactory() {}
 
@@ -38,5 +99,11 @@ ConsoleFactory ConsoleFactory::make(std::istream &in, std::ostream &out) {
   return ConsoleFactory{std::move(console)};
 }
 
-bool ConsoleFactory::start() const { return get()->start(); }
+bool ConsoleFactory::start() const {
+  Console &console = *get();
+  auto nop = console.addCommand("nop", [](auto &, auto &) {});
+  auto exit = console.addCommand("exit", [](auto &, auto &) { return false; });
+  auto r = console.addCommand("restart", [](auto &, auto &) { return true; });
+  return console.start();
+}
 }
